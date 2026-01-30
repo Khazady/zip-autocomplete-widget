6edec1b2179fd4eeb1bf24ced655e7e739fa95c2
Michael_Kalatsey <mkalatsey@remedypointsolutions.com>
2026-01-30 11:58:54 +0100

    feat(dependencies): install and config SVGR

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,27 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "/zip-autocomplete-widget/",
-  plugins: [react()],
+  plugins: [
+    react(),
+    svgr({
+      svgrOptions: {
+        plugins: ["@svgr/plugin-svgo", "@svgr/plugin-jsx"],
+        svgoConfig: {
+          floatPrecision: 2,
+          plugins: [
+            {
+              name: "cleanupIds",
+            },
+            { name: "convertPathData" },
+          ],
+        },
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": "/src",
