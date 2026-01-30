715ee67b6e3159f14a01b49c11ed67a04684bc51
Michael_Kalatsey <mkalatsey@remedypointsolutions.com>
2026-01-30 14:06:17 +0100

    feat(dictionary): add texts to dictionary

---
+++ src/lib/dictionary.ts
@@ -0,0 +1,15 @@
+export const dictionary = {
+  app: {
+    scrollHint: "Scroll down",
+    fallbackLoading: "Loading availability...",
+  },
+  widget: {
+    launcher: "Check availabilities",
+    header:
+      "Let us know your postal code so we can get you\nbest cleaning professionals in your area.",
+    placeholder: "Your postal code or city",
+    buttonLabel: "Lets go!",
+    empty: "No matches yet. Try another zip.",
+    error: "Couldn't load matches.",
+  },
+};
