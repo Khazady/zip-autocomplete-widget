2f8a68e2701495131cf66faa1d9c2d0f7c077560
Michael_Kalatsey <mkalatsey@remedypointsolutions.com>
2026-01-30 14:10:49 +0100

    feat(spinner): add Spinner component

---
+++ src/components/ui/Spinner/Spinner.tsx
@@ -0,0 +1,12 @@
+import styles from "@/components/ui/Spinner/Spinner.module.css";
+import {cn} from "@/lib/utils/cn";
+
+type SpinnerProps = {
+  className?: string;
+};
+
+export const Spinner = ({ className }: SpinnerProps) => {
+  return (
+    <span className={cn(styles.spinner, className)} />
+  );
+};
