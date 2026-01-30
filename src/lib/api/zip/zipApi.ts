b636b69be5be818189f03802f7900083860e6c58
Michael_Kalatsey <mkalatsey@remedypointsolutions.com>
2026-01-30 14:32:35 +0100

    feat(zip): implement delay in getZipCodes func to simulate loading and show loading state

--- src/lib/api/zip/zipApi.ts
+++ src/lib/api/zip/zipApi.ts
@@ -1,11 +1,14 @@
 import type {ZipApiRecord} from "@/lib/api/zip/zipTypes";
 
 const DATA_URL = `${import.meta.env.BASE_URL}mocked-data.json`;
+const SHOULD_DELAY = true;
 
 export const getZipCodes = async (): Promise<ZipApiRecord[]> => {
-  await new Promise((resolve) => {
-    setTimeout(resolve, 600);
-  });
+  if (SHOULD_DELAY) {
+    await new Promise((resolve) => {
+      setTimeout(resolve, 1000);
+    });
+  }
   const response = await fetch(DATA_URL);
   if (!response.ok) {
     throw new Error("Failed to fetch zip data");
